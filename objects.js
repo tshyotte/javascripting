01fd66877d7752ef194f508de59c4cdc774ba63c
Tshitenga Bahome <odettebahome@gmail.com>
2020-09-19 20:14:06 +0000

    array objects added

---
+++ objects.js
@@ -0,0 +1,8 @@
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+};
+console.log("Toppings: " + pizza.toppings);
+console.log("Crust: " + pizza.crust);
+console.log("Serves: " + pizza.serves);
